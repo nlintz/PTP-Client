a5d760037220ebb6c4e60833f5d6683b883278b5
Nathan Lintz <nlintz@gmail.com>
2014-08-05 18:05:41 -0700

    added config writer

--- index.js
+++ index.js
@@ -4,52 +4,52 @@ var PTPClient = require('./lib/ptp_client.js'),
     SCPConnection = require('./lib/scp_connection.js'),
     CommandExecutor = require('./lib/command_executor.js'),
     Commands = require('./lib/command_builder.js'),
-    config = require('./config.js'),
+    configWriter = require('./lib/config_writer.js'),
     cli = require('cli').setUsage('PTP run/gitdeploy/sshdeploy <Path To Script Relative To Project Directory>'),
     options = cli.parse({
       verbose: ['v', 'verbose'],
     });
 
-// Create a remote connection service (scpConn) and a shell command service (commandExecutor)
-var scpConn = new SCPConnection(config.host, config.username, config.password, config.remoteDirectory);
-var commandExecutor = new CommandExecutor(config.host, config.username, config.sshPub);
-
 cli.main(function (args, options) {
-  // Main 
-  if (args.length < 2) {
-    console.log("PTP Requires At Least Three Arguments. Type PTP -h To See Usage");
-    return;
-  };
+  // Configure PTP
+  if (args[0] == "config") {
+    configWriter.config();
+  } else {
 
-  // By Default PTP deploys a tarball via SSH
-  if (args[0] == "run") {
-    var ptpClient = new PTPClient(scpConn, commandExecutor);
-    var re = new RegExp("([^\/]*\/+)(.*)");
-    var scriptPath = args[1];
-    var localDirectory = scriptPath.match(re)[1]
-    var scriptPath = scriptPath.match(re)[2]
-    
-    ptpClient.shell(Commands.runScript(localDirectory, config.remoteDirectory, scriptPath), {verbose:options.verbose})
-  }
+  // Create a remote connection service (scpConn) and a shell command service (commandExecutor)
+    var config = require('./config.json'),
+        scpConn = new SCPConnection(config.host, config.username, config.password, config.remoteDirectory),
+        commandExecutor = new CommandExecutor(config.host, config.username, config.sshPub);
 
-  else if (args[0] == "sshdeploy") {
-    var ptpClient = new PTPClient(scpConn, commandExecutor);
-    var re = new RegExp("([^\/]*\/+)(.*)");
-    var scriptPath = args[1];
-    var localDirectory = scriptPath.match(re)[1]
-    var scriptPath = scriptPath.match(re)[2]
+    if (args[0] == "run") {
+      var ptpClient = new PTPClient(scpConn, commandExecutor);
+      var re = new RegExp("([^\/]*\/+)(.*)");
+      var scriptPath = args[1];
+      var localDirectory = scriptPath.match(re)[1]
+      var scriptPath = scriptPath.match(re)[2]
+      
+      ptpClient.shell(Commands.runScript(localDirectory, config.remoteDirectory, scriptPath), {verbose:options.verbose})
+    }
 
-    ptpClient.send(localDirectory, config.projectArchive, function () {
-      ptpClient.shell(Commands.sshDeploy(localDirectory, config.projectArchive, config.remoteDirectory, scriptPath), {verbose:options.verbose})
-    })
-  }
+    else if (args[0] == "sshdeploy") {
+      var ptpClient = new PTPClient(scpConn, commandExecutor);
+      var re = new RegExp("([^\/]*\/+)(.*)");
+      var scriptPath = args[1];
+      var localDirectory = scriptPath.match(re)[1]
+      var scriptPath = scriptPath.match(re)[2]
 
-  // You can also deploy with git
-  else if (args[0] == "gitdeploy") {
-    var ptpClient = new PTPClient(null, commandExecutor);
-    var re = new RegExp(".*\/(.*).git$")
-    var giturl = args[1];
-    var repo = giturl.match(re)[1];
-    var deploy = ptpClient.shell(Commands.gitDeploy(giturl, repo, config.remoteDirectory), {verbose:options.verbose});
+      ptpClient.send(localDirectory, config.projectArchive, function () {
+        ptpClient.shell(Commands.sshDeploy(localDirectory, config.projectArchive, config.remoteDirectory, scriptPath), {verbose:options.verbose})
+      })
+    }
+
+    // You can also deploy with git
+    else if (args[0] == "gitdeploy") {
+      var ptpClient = new PTPClient(null, commandExecutor);
+      var re = new RegExp(".*\/(.*).git$")
+      var giturl = args[1];
+      var repo = giturl.match(re)[1];
+      var deploy = ptpClient.shell(Commands.gitDeploy(giturl, repo, config.remoteDirectory), {verbose:options.verbose});
+    }
   }
 });
